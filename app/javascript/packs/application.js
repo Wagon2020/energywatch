20c36f51f5bed4d7d06fe4ea99805b011c480437
Stefan Sarow <stefan@pixelaffairs.com>
2020-06-02 17:28:37 +0200

    api test2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,7 +24,6 @@ require("channels")
 
 // External imports
 import "bootstrap";
-import { getDataFromApi } from "../api/api_request"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,5 +31,4 @@ import { getDataFromApi } from "../api/api_request"
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  getDataFromApi();
 });
--- app/services/entsoe_api.rb
+++ app/services/entsoe_api.rb
@@ -3,20 +3,20 @@ require 'open-uri'
 
 class EntsoeApi
   def initialize
-    @base_url = "https://transparency.entsoe.eu/"
+    @base_url = "https://transparency.entsoe.eu/api?"
     @token = "securityToken=dfb276c1-986a-4e92-a736-74d584bb782c"
     @doc_type = "A75"
     @process_type = "A16"
     @psr = "B18"
     @domain = "10Y1001A1001A83F"
-    @p_start = "202006021200"
+    @p_start = "202006010000"
     @p_end = "202006012300"
     @actual = "https://transparency.entsoe.eu/api?securityToken=dfb276c1-986a-4e92-a736-74d584bb782c&documentType=A75&processType=A16&psrType=B18&in_Domain=10Y1001A1001A83F&periodStart=202006010000&periodEnd=202006012300"
-    @url = "#{@base_url}#{@token}&documentType=#{@doc_type}&processType=#{@psr_type}&psrType=#{@psr}&in_Domain=#{@domain}&periodStart=#{@start}&periodEnd=#{@p_end}"
+    @url = "#{@base_url}#{@token}&documentType=#{@doc_type}&processType=#{@process_type}&psrType=#{@psr}&in_Domain=#{@domain}&periodStart=#{@p_start}&periodEnd=#{@p_end}"
   end
 
   def call
-    url = open(@actual)
+    url = open(@url)
     data = Hash.from_xml(url)
     data["GL_MarketDocument"]
     time_period = data["GL_MarketDocument"]["time_Period.timeInterval"]
@@ -25,9 +25,13 @@ class EntsoeApi
     entry_array = data["GL_MarketDocument"]["TimeSeries"]["Period"]["Point"]
 
     p data
-    binding.pry
+    # binding.pry
+    data
   end
 end
+
+
+
 
 # document = Nokogiri::XML(url)
 # puts document

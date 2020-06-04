0430c0a734863827c5f380d514a49e396f5f861c
Stefan Sarow <stefan@pixelaffairs.com>
2020-06-04 13:09:51 +0200

    controller calls updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :data]
 
   def home
-    @data = EntsoeApi.new.energy_types
-    @donut = EntsoeApi.new.donut
+    @data = EntsoeData.energy_types
+    @donut = EntsoeData.donut
     @donut_array = []
     @donut.each do |fueltype, quantity|
       @donut_array << [fueltype, quantity]
@@ -20,8 +20,8 @@ class PagesController < ApplicationController
   end
 
   def data
-    @data = EntsoeApi.new.energy_types
-    @donut = EntsoeApi.new.donut
+    @data = EntsoeData.energy_types
+    @donut = EntsoeData.donut
     @donut_array = []
     @donut.each do |fueltype, quantity|
       @donut_array << [fueltype, quantity]
@@ -32,10 +32,6 @@ class PagesController < ApplicationController
       @chart_array << [fueltype, quantity]
     end
     gon.chart_data = @chart_array
-  end
-
-  def store
-    EntsoeApi.new.store_latest_call
   end
 
 end

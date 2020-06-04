aa1213ba32823d60028b8a8237430cf804954b53
leo huettig <learntocode2020@protonmail.com>
2020-06-04 19:10:01 +0200

    home nav no shadow

--- app/javascript/packs/scroll_event.js
+++ app/javascript/packs/scroll_event.js
@@ -3,7 +3,7 @@ const scroller = () => {
   if (navbar) {
     window.addEventListener('scroll', (event) => {
       navbar.style.background = `rgba(31, 229, 189, ${event.pageY * (1 / 140)})`;
-      // navbar.style.opacity = `${event.pageY * (1.14)}`
+      navbar.style.boxShadow = "box-shadow: 0 0 15px rgba(0,0,0,0.2)";
       console.log(event.pageY);
     });
   }

66530921e9f3f86861b602e66ac4924554e60d15
leo huettig <learntocode2020@protonmail.com>
2020-06-08 11:46:00 +0200

    setup pre domain

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,9 +1,17 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# class UserMailerPreview < ActionMailer::Preview
+
+#   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
+#   def welcome
+#     UserMailer.welcome
+#   end
+
+# end
+
 class UserMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
   def welcome
-    UserMailer.welcome
+    user = User.first
+    # This is how you pass value to params[:user] inside mailer definition!
+    UserMailer.with(user: user).welcome
   end
-
 end

0ffad94a149b38567b1d91c23f6388a8d703a8fa
Stefan Sarow <stefan@pixelaffairs.com>
2020-06-09 11:09:37 +0200

    test mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,12 @@
 Rails.application.configure do
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.default_url_options = { host: "http://energywatch.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  # config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -38,7 +39,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 

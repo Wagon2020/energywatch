10f26e798dbcb2f28672f30efe89c30e2a58c5c0
Ailin Huang <ailin.h58@gmail.com>
2020-06-09 15:45:41 +0200

    clickable button on homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,8 +32,6 @@ import { scroller } from '../components/scroll_event';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-
-
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();

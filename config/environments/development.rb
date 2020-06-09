5fa2307028abfe95ae89c88803af0360c904e3b8
leo huettig <learntocode2020@protonmail.com>
2020-06-09 11:54:33 +0200

    send email in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
+  config.action_mailer.perform_deliveries = true
   # config.action_mailer.default_url_options = { host: "http://localhost:3000" }
 
   # Settings specified here will take precedence over those in config/application.rb.

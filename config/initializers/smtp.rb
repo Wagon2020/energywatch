6080fb6ff1be12fc699f750f14d13396447eb47e
Stefan Sarow <stefan@pixelaffairs.com>
2020-06-09 11:18:57 +0200

    test mail hgeroku

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address: "mail.privateemail.com",
   port: 465,
   ssl: true,
-  domain: 'www.privateemail.com',
+  domain: 'www.privateemail123.com',
   user_name: ENV['MAIL_ADDRESS'],
   password: ENV['MAIL_APP_PASSWORD'],
   authentication: :login,

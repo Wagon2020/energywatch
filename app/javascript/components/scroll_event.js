efd50e1b6e7c568b62db805fc062c058f811ed8d
leo huettig <learntocode2020@protonmail.com>
2020-06-10 17:12:59 +0200

    longer navbar transition time

--- app/javascript/components/scroll_event.js
+++ app/javascript/components/scroll_event.js
@@ -2,10 +2,10 @@ const scroller = () => {
   let navbar = document.querySelector('.home_nav');
   if (navbar) {
     window.addEventListener('scroll', (event) => {
-      const scrollValue = $(window).scrollTop() / (140)
+      const scrollValue = $(window).scrollTop() / (240)
       navbar.style.background = `rgba(97, 71, 255, ${scrollValue})`;
       navbar.style.boxShadow = `0 0 15px rgba(0,0,0,${Math.min(scrollValue / 5, 0.2)})`;
-      // console.log(scrollValue);
+      console.log(scrollValue);
     });
   }
 }
